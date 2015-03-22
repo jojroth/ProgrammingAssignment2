c6e29cfc6705fb94e2586bf4b0aaf18ad4077818
jojroth <jjroth@web.de>
2015-03-22 20:01:02 +0100

    Produce functions makecachematrix and solvecachematrix
    reduces computing time if inverse matrix has to be computed several times

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+mat## The functions contained in this file may help reduce processing time
+## when it is necessary to repeatedly calculate the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCachematrix: creates a matrix object that caches its inverse
+## has to be run prio to cachesolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL ## assign Null to matrix m, for later check wether inverse has already been calculated
+  set <- function(y) {
+    x <<- y
+    m <<- NULL ## assign Null to matrix in parent environment
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve ## assigns inverse matrix to m
+  getinverse <- function() m
+  list(set = set, get = get, 
+       setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cachesolve: computes the inverse of the matrix created by makeCachematrix
+## if this has already been calculated and if the matrix is unchanged, 
+## then cachesole retrieves the inverse from the cache instead of calculating it again.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {  ## in case inverse has already been computed
+    message("getting cached inverse matrix")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(data) ## calculate inverse matrix and assign value to m
+  x$setinverse(m) ## assigns inverse matrix to m
+  m
 }
